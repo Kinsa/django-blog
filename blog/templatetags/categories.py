8f2d3095ec5641b6a98472784479e671eff17fe3
Joseph Bergantine <jbergantine@gmail.com>
2012-10-06 15:18:00 -0600

    moved categories assignment out of try/except loop so it always returns

--- blog/templatetags/categories.py
+++ blog/templatetags/categories.py
@@ -8,9 +8,9 @@ register = template.Library()
 
 @register.inclusion_tag('blog/_categories.html')
 def fetch_categories():
+    categories = []
     try:
         all_categories = Category.objects.all()
-        categories = []
         for c in all_categories:
             if len(c.live_entry_set()) > 0:
                 categories.append(c)
