3ec222a77ed2dc6a746b24190e94045f76c0e51e
Joseph Bergantine <jbergantine@gmail.com>
2013-01-02 21:03:45 -0700

    Class based YearArchiveView.

--- django_blog/urls/entries.py
+++ django_blog/urls/entries.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 from django_blog.models import Entry
-from django.views.generic.dates import ArchiveIndexView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView
 
 
 entry_info_dict = {
@@ -15,12 +15,13 @@ entry_detail_dict = {
 }
 
 urlpatterns = patterns('django.views.generic.date_based',
-    url(r'^$', ArchiveIndexView.as_view(queryset=Entry.live.all(),
-        date_field='pub_date'), name='blog_entry_archive'),
-    (r'^(?P<year>\d{4})/$',
-        'archive_year',
-        dict(entry_info_dict, make_object_list=True),
-        'blog_entry_archive_year'),
+    url(r'^$', ArchiveIndexView.as_view(date_field='pub_date',
+        queryset=Entry.live.all()), name='blog_entry_archive'),
+
+    url(r'^(?P<year>\d{4})/$', YearArchiveView.as_view(date_field='pub_date',
+        make_object_list=True, queryset=Entry.live.all()),
+        name='blog_entry_archive_year'),
+
     (r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
         'archive_month',
         entry_info_dict,
