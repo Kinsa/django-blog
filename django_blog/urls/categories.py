3233e351c91408b18f05d80c3f4c6e7d290e1e0a
Joseph Bergantine <jbergantine@gmail.com>
2013-01-02 21:21:33 -0700

    Generic list view.

--- django_blog/urls/categories.py
+++ django_blog/urls/categories.py
@@ -1,13 +1,14 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
+from django.views.generic.list import ListView
 
 from django_blog.models import Category
 
 
 urlpatterns = patterns('',
-    (r'^$',
-        'django.views.generic.list_detail.object_list',
-        {'queryset': Category.objects.all()},
-        'blog_category_list'),
+    url(r'^$',
+        ListView.as_view(queryset=Category.objects.all()),
+        name='blog_category_list'
+    ),
     (r'^(?P<slug>[-\w]+)/$',
         'django_blog.views.category_detail',
         '',
