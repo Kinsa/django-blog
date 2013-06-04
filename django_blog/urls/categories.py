e8db5ad8e0ae4574ff238909888bdafcc4ec9edc
Joseph Bergantine <jbergantine@gmail.com>
2013-06-04 15:09:40 -0500

    Update categories.py

--- django_blog/urls/categories.py
+++ django_blog/urls/categories.py
@@ -7,9 +7,8 @@ from django_blog.models import Category
 urlpatterns = patterns('',
     url(r'^$',
         ListView.as_view(queryset=Category.objects.all()),
-        name='blog_category_list'
-    ),
-    (r'^(?P<slug>[-\w]+)/$',
+        name='blog_category_list'),
+    url(r'^(?P<slug>[-\w]+)/$',
         'django_blog.views.category_detail',
-        '',
-        'blog_category_detail'),)
+        name='blog_category_detail'),
+)
